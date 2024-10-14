154cba164c716cc39917cf1b1b1fae56654e778f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-14 19:50:22 +0530

    fix: delete-webpack-yml

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-// import { Experience } from "./components/Experience";
+import { Experience } from "./components/Experience";
 import { UI } from "./components/UI";
 
 function App() {
