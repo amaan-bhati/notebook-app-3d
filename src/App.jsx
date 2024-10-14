91390b220470d25146cec9fbef4f855d624b23c8
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-14 18:14:58 +0530

    component: expereince-omponent

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ function App() {
       <UI />
       <Loader />
       <Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
+        <group position-y={0}>
+          <Suspense fallback={null}>
+            <Experience />
+          </Suspense>
+        </group>
       </Canvas>
     </>
   );
