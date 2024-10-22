b05287abe7f762199775db99a2cf23121cce2f46
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-22 22:35:03 +0530

    graphics: set-shadows-and-bones-structure

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,13 @@
 import { useMemo, useRef } from "react";
 import { pages } from "./UI";
-import { Bone, BoxGeometry, Skeleton, SkinnedMesh, Uint16BufferAttribute, Vector3 } from "three";
+import {
+  Bone,
+  BoxGeometry,
+  Skeleton,
+  SkinnedMesh,
+  Uint16BufferAttribute,
+  Vector3,
+} from "three";
 
 const PAGE_WIDTH = 1.28;
 const PAGE_HEIGHT = 1.71;
@@ -16,39 +23,35 @@ const pageGeometry = new BoxGeometry(
   2
 );
 
-pageGeometry.translate(PAGE_WIDTH/ 2, 0, 0);//set page geometry to shift it to a relevant position
+pageGeometry.translate(PAGE_WIDTH / 2, 0, 0); //set page geometry to shift it to a relevant position
 const position = pageGeometry.attributes.position;
 const vertex = new Vector3();
 const skinIndexes = [];
 const skinWeights = [];
 
-for ( let i = 0; i < position.count; i++){
+for (let i = 0; i < position.count; i++) {
   //ITERATING ALL THE VERTICES VERTEX.
-  vertex.fromBufferAttribute(position, i);//get the vertex
-  const x = vertex.x;// get the x position of the vertex
+  vertex.fromBufferAttribute(position, i); //get the vertex
+  const x = vertex.x; // get the x position of the vertex
 
   const skinIndex = Math.max(0, Math.floor(x / SEGMENT_WIDTH));
-  let skinWeight = (x % SEGMENT_WIDTH) / SEGMENT_WIDTH;//setting up the skin indexes
+  let skinWeight = (x % SEGMENT_WIDTH) / SEGMENT_WIDTH; //setting up the skin indexes
 
-  skinIndex.push(skinIndex, skinIndex + 1, 0, 0);//we use two bones per vertex here
+  skinIndex.push(skinIndex, skinIndex + 1, 0, 0); //we use two bones per vertex here
 
-  skinWeights.push(1 - skinWeight, skinWeight, 0, 0);//impact of the bone
+  skinWeights.push(1 - skinWeight, skinWeight, 0, 0); //impact of the bone
 
   //attaching the skin wieght to the geometry
 }
-  pageGeometry.setAttribute(
-    "skinIndex",
-    new Uint16BufferAttribute(skinIndexes, 4)
-  );
+pageGeometry.setAttribute(
+  "skinIndex",
+  new Uint16BufferAttribute(skinIndexes, 4)
+);
 
-  pageGeometry.setAttribute(
-    "skinIndex",
-    new Float32BufferAttribute(skinWeights, 4)
-  );//float because it is a value between 0 and 1
-
-
-
-
+pageGeometry.setAttribute(
+  "skinIndex",
+  new Float32BufferAttribute(skinWeights, 4)
+); //float because it is a value between 0 and 1
 
 //page component
 
@@ -57,25 +60,23 @@ const Page = ({ number, front, back, ...props }) => {
   const SkinnedMeshRef = useRef();
 
   const manualSkinnedMesh = useMemo(() => {
-
     const bones = [];
 
-    for(let i = 0; i<= PAGE_SEGMENTS; i++){
+    for (let i = 0; i <= PAGE_SEGMENTS; i++) {
       //we'll have as many bones as the segments
 
       let bone = new Bone();
       bones.push(bone);
       //for each segment we need to have a new bone
 
-      if (i === 0){
+      if (i === 0) {
         bone.position.x = 0;
-      }
-      else {
+      } else {
         bone.position.x = SEGMENT_WIDTH;
       }
-      if (i > 0){
+      if (i > 0) {
         bones[i - 1].add(bone);
-        //attach the new bone to the previous  
+        //attach the new bone to the previous
       }
     }
 
@@ -88,14 +89,7 @@ const Page = ({ number, front, back, ...props }) => {
     mesh.add(skeleton.bones[0]);
     mesh.bind(skeleton);
     return mesh;
-
-
-
-
-
   }, []);
-
-
 
   return (
     <group {...props} ref={group}>
